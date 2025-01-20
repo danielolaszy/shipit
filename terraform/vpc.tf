50b5bea71fc414bf83208736c4f4a96c5ce313b7
Daniel Olaszy <daniel.olaszy@gmail.com>
2025-01-20 11:50:21 +0000

    chore: edit security group for HTTPS

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -68,6 +68,14 @@ resource "aws_security_group" "shipit_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTPS from any IP address"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
